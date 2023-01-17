56070a4bdb348ea436f9d059c9d56070327532ae
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2023-01-17 18:58:53 +0900

    add comments on problem4.

--- week1/src/main/java/onboarding/Problem4.java
+++ week1/src/main/java/onboarding/Problem4.java
@@ -1,8 +1,38 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem4 {
+
+    /**
+     * This method will make inputs into another sentence
+     * by following specific rules that explained in problem.
+     * Put the "word" arguments into ArrayList.
+     * This ArrayList would be the parameter of changeWord method.
+     * changeWord method would return the answer.
+     * @param word that should be turned into other word.
+     * @return the sentence made based on the parameter "word"
+     */
     public static String solution(String word) {
         String answer = "";
+
+        return answer;
+    }
+
+    /**
+     * First, this method will distinguish between uppercase and lowercase.
+     * Based on ASCII code, each alphabet's ASCII code differs at most 25.
+     * This method will find how far the target letter is from the a(or A).
+     * A's ASCII code is 65. If the target letter is E, whose ASCII code is 69.
+     * Distance between A and E is 4.
+     * This method will subtract 4 from 90(because it's Z's ASCII code).
+     * Then E would be changed into V.
+     * @param wordList The given word changed into ArrayList.
+     * @return the String changed into specific rules explained in the problem.
+     */
+    private static String changeWord(ArrayList<Character> wordList) {
+        String answer = "";
+
         return answer;
     }
 }
